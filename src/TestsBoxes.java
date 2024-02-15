e5b897848188026ac5824cba2b45b01bf32c1da5
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-02-15 11:39:54 +0100

    Ajout de Boxes.java Ajout de TestsBoxes.java

---
+++ src/TestsBoxes.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+import javax.swing.Box;
+
+public class TestsBoxes {
+    @Test
+    public void testBoxCreate() {
+        Box b = new Box();
+    }
+    
+}
